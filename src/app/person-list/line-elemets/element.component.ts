26d253e2478415572a1b21cbed32364182dc57a1
Vladislav Zinchuk <zima04zima@gmail.com>
2017-07-12 20:02:01 +0300

    Remove unnecessary components.

--- src/app/person-list/line-elemets/element.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'line-elements',
-  templateUrl: 'element.component.html',
-  styleUrls: ['element.component.scss'],
-})
-export class LineElementsComponent {}
