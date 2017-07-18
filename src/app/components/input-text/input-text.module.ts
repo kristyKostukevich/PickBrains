2ae710e8e91303c122dc78688e40739558c68c71
Maxim Aderiha <maxima290898@gmail.com>
2017-07-18 11:09:57 +0300

    Refactor input-text component.

--- src/app/components/input-text/input-text.module.ts
+++ src/app/components/input-text/input-text.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MdInputModule } from '@angular/material';
 import { InputTextComponent } from './input-text.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
