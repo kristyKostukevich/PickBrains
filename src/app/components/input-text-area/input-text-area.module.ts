84b1480a6f45d69e803c8fd632c932f292c6e7db
Maxim Aderiha <maxima290898@gmail.com>
2017-07-18 11:07:52 +0300

    Refactor input-text-area component.

--- src/app/components/input-text-area/input-text-area.module.ts
+++ src/app/components/input-text-area/input-text-area.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MdInputModule } from '@angular/material';
 import { InputTextAreaComponent } from './input-text-area.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    MdInputModule,
     FormsModule,
+    MdInputModule,
   ],
   declarations: [InputTextAreaComponent],
   exports: [InputTextAreaComponent],
