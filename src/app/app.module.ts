516229028b2906c31ca736a3e2b0ca1863550402
Maxim Aderiha <maxima290898@gmail.com>
2017-07-18 11:27:46 +0300

    Refactor.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GlobalHeaderModule } from './components/global-header/header.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCheckboxModule, MaterialModule } from '@angular/material';
-import { PersonPageModule } from './person-list/person-list-component/person-list.module';
-import { VacanciesDetailModule } from "./vacancy-detail/vacancy-detail-wrapper/vacancy-detail-wrapper.module";
-import { VacancyDetailMenuModule } from './vacancy-detail/vacancy-detail-menu/vacancy-detail-menu.module';
+import { GlobalHeaderModule } from './components/header/header.module';
+import { VacanciesDetailModule } from './vacancy-detail/vacancy-detail-wrapper/vacancy-detail-wrapper.module';
+import { AppComponent } from './app.component';
 
 
 @NgModule({
   imports: [
-    MaterialModule,
     BrowserAnimationsModule,
-    MdButtonModule,
-    MdCheckboxModule,
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule,
     GlobalHeaderModule,
     VacanciesDetailModule,
-    PersonPageModule,
-    VacancyDetailMenuModule,
   ],
   declarations: [
     AppComponent,
@@ -32,5 +21,7 @@ import { VacancyDetailMenuModule } from './vacancy-detail/vacancy-detail-menu/va
   providers: [],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {
+
 }
