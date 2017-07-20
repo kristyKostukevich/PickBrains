df27d59af3731e99f37045d5107a63130c6e7935
Maxim Aderiha <maxima290898@gmail.com>
2017-07-20 15:13:29 +0300

    View vac-det-page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GlobalHeaderModule } from './components/header/header.module';
-import { VacanciesDetailModule } from './vacancy-detail/vacancy-detail.module';
+import { VacancyDetailModule } from './vacancy-detail/vacancy-detail.module';
 import { AppComponent } from './app.component';
 import { AccordionModule } from 'ngx-accordion';
 import { PersonPageModule } from './main-page/person-list-component/person-list.module';
 import { HttpModule } from '@angular/http';
 import { TempModule } from './Temp/temp.module';
 import { FilterPersonModule } from './components/filter-components/filter-person/filter-person.module';
-import { VacancyListComponent } from './main-page/vacancy-list-component/vacancy-list-component';
 import { VacancyPageModule } from './main-page/vacancy-list-component/vacancy-list-component.module';
 import { HistoryModule } from './components/history/history.module';
 import { HistoryPageModule } from './main-page/history-list-component/history-list-component.module';
@@ -28,6 +27,7 @@ import { HistoryPageModule } from './main-page/history-list-component/history-li
     FilterPersonModule,
     VacancyPageModule,
     HistoryPageModule,
+    VacancyDetailModule,
   ],
   declarations: [
     AppComponent,
