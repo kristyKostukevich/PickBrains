45c2508b4519ad461a2c457fb582f947a711bb1c
Maxim Aderiha <maxima290898@gmail.com>
2017-07-21 02:50:25 +0300

    Changed interface to class.

--- src/app/interfaces/select-model.ts
+++ src/app/interfaces/select-model.ts
@@ -1,5 +1,11 @@
-export interface SelectModel {
+export class SelectModel {
   value: string;
   placeholder: string;
   options: string[];
+
+  constructor(value: string, placeholder: string, options: string[]) {
+    this.value = value;
+    this.placeholder = placeholder;
+    this.options = options;
+  }
 }
