99c24418b01359267e3ec423bd85b2b444db6046
Maxim Aderiha <maxima290898@gmail.com>
2017-07-27 15:46:06 +0300

    Update select component.

--- src/app/interfaces/select-model.ts
+++ src/app/interfaces/select-model.ts
@@ -2,10 +2,14 @@ export class SelectModel {
   value: string;
   placeholder: string;
   options: string[];
+  level: number;
+  id: number;
 
-  constructor(value: string, placeholder: string, options: string[]) {
+  constructor(value: string, placeholder: string, options: string[], level?: number, id?: number) {
     this.value = value;
     this.placeholder = placeholder;
     this.options = options;
+    this.level = level || 0;
+    this.id = id || 0;
   }
 }
