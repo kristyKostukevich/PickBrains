1cb7af2aefa2ce4b60ea7ab10e3e6355d6c2aadd
Maxim Aderiha <maxima290898@gmail.com>
2017-07-27 15:49:53 +0300

    Update filter menu.

--- src/app/main-page/menu-filter-component/menu-filter.component.ts
+++ src/app/main-page/menu-filter-component/menu-filter.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'person-page-filter',
   templateUrl: 'menu-filter.component.html',
   styleUrls: ['menu-filter.component.scss'],
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit {
+  toggleFlags: boolean[];
+  items: string[] = ['Person', 'Vacancy', 'History'];
+
+  ngOnInit() {
+    this.toggleFlags = [];
+    this.toggleFlags[0] = true;
+  }
+
+  toggle(item: string) {
+    console.log(item);
+    let index: number = 0;
+    for (const i of this.items) {
+      this.toggleFlags[index] = false;
+      if (i === item) {
+        this.toggleFlags[index] = true;
+      }
+      index += 1;
+    }
+  }
 }
 
