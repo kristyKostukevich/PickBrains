b863cddb9ce57b716678398f6a1efb4acaad079d
Maxim Aderiha <maxima290898@gmail.com>
2017-07-27 14:24:25 +0300

    Update menu component.

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,21 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { ShowMenuService } from 'app/components/header/show-menu.service';
 
 @Component({
   selector: 'menu',
   templateUrl: 'menu.component.html',
   styleUrls: ['menu.component.scss'],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnChanges {
   @Input() menuItems: string[];
+  @Input() id: number;
+  @Input() type: string;
+  flag: boolean = false;
+
   toggleFlags: boolean[];
 
-  ngOnInit() {
+  constructor(private router: Router, private showMenu: ShowMenuService) {
     this.toggleFlags = [];
-    this.toggleFlags[0] = true;
+    this.showMenu.getEmitter().subscribe(flag => {
+      this.flag = flag;
+    });
+  }
+
+  ngOnChanges() {
+  }
+
+  ngOnInit() {
+    this.goToItem('General');
   }
 
   toggle(item: string) {
-    console.log(item);
+    if (document.documentElement.clientWidth <= 800) {
+      this.showMenu.menuToggle();
+    }
+
     let index: number = 0;
     for (const i of this.menuItems) {
       this.toggleFlags[index] = false;
@@ -24,5 +42,33 @@ export class MenuComponent implements OnInit {
       }
       index += 1;
     }
+  }
+
+  getParent() {
+    console.log(this.type);
+    switch (this.type) {
+      case 'candidate':
+        return '/person-page/detail-candidate';
+      case 'vacancy':
+        return '/vacancy-page/detail-vacancy';
+      default:
+        break;
+    }
+  }
+
+  goToItem(item: string) {
+    this.toggle(item);
+    const parentPath = this.getParent();
+    const path = `${parentPath}/${this.id}/${item.replace(/\s/g, '-')
+      .toLowerCase()}`;
+    this.router.navigate([path],
+      {
+        queryParams: {
+          itemType: item,
+          type: this.type,
+          id: this.id,
+        },
+      },
+    );
   }
 }
--- src/app/components/menu/menu.module.ts
+++ src/app/components/menu/menu.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MdButtonModule } from '@angular/material';
 import { MenuComponent } from './menu.component';
+import { AppRoutingModule } from '../../app-routing.module';
+import { ShowMenuService } from 'app/components/header/show-menu.service';
 
 @NgModule({
   imports: [
+    AppRoutingModule,
     CommonModule,
     MdButtonModule,
   ],
   declarations: [MenuComponent],
   exports: [MenuComponent],
+  providers: [ShowMenuService,
+  ],
 })
 export class MenuModule {
 }
