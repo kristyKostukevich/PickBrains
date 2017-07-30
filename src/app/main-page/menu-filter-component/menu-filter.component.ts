521f4e433dda853ec5496afb299a446ffc0a68b8
Kristina <AMashKos@yandex.by>
2017-07-30 19:55:03 +0300

    limitation for HRM

--- src/app/main-page/menu-filter-component/menu-filter.component.ts
+++ src/app/main-page/menu-filter-component/menu-filter.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthorizationService } from '../../authorization/authorization.service';
+import { HttpService } from '../../http-service/http-service';
+
 
 @Component({
   selector: 'person-page-filter',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['menu-filter.component.scss'],
 })
 export class FilterComponent {
+  user: string;
+  constructor(private authorization: AuthorizationService, private http: HttpService) {
+    this.authorization.getEmitter().subscribe(data =>
+      this.user = data.type);
+  }
+
+  ngOnInit() {
+    this.http
+      .getData(`http://localhost:1337/api/user`)
+      .subscribe((data) => {
+        this.user = data.json().type;
+      });
+  }
+
+
 }
 
