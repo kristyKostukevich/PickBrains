2712f3a1a85f4653da75640fe8dda14ae41bd99a
Kristina <AMashKos@yandex.by>
2017-07-31 14:13:33 +0300

    add localStorage

--- src/app/guards/tech.guard.ts
+++ src/app/guards/tech.guard.ts
@@ -1,33 +1,19 @@
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 import { Injectable } from '@angular/core';
-import { HttpService } from '../http-service/http-service';
+
 
 @Injectable()
 export class TechGuard implements CanActivate{
-  constructor(private http: HttpService) {}
-
-  typeUser(): string {
-    this.http
-      .getData(`http://localhost:1337/api/user`)
-      .subscribe((data) => {
-        console.log(data.json().type);
-        return data.json().type;
-      });
-
+  user: string;
+  constructor() {
+    const data = JSON.parse(localStorage.getItem('user'));
+    this.user = data.type;
   }
-
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
-  Observable<boolean> | boolean {
-    this.http
-      .getData(`http://localhost:1337/api/user`)
-      .subscribe((data) => {
-        console.log(data.json().type);
-        console.log(data.json().type === 'admin');
-        return data.json().type === 'admin';
-      });
-    //console.log((this.typeUser() === 'HRM'));
-    //return (this.typeUser() === 'admin' || this.typeUser() === 'HRM');
+    Observable<boolean> | boolean {
+    console.log(this.user === 'admin' || this.user === 'HRM');
+    return this.user === 'admin' || this.user === 'HRM';
   }
 
 }
