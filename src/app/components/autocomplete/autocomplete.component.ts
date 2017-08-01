eefa7a0c07bd74ae901257c14d0be98b1be29f1d
Maxim Aderiha <maxima290898@gmail.com>
2017-08-02 02:06:27 +0300

    Other changes.

---
+++ src/app/components/autocomplete/autocomplete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'autocomplete.component.scss',
+  templateUrl: 'autocomplete.component.html',
+})
+
+export class AutocompleteComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
