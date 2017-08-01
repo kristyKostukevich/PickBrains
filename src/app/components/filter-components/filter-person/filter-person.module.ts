69615f2032eb3c90121d363bdbf1e89a1eda9a3e
Vladislav Zinchuk <zima04zima@gmail.com>
2017-08-01 18:55:59 +0300

    make filter with new components

--- src/app/components/filter-components/filter-person/filter-person.module.ts
+++ src/app/components/filter-components/filter-person/filter-person.module.ts
@@ -4,9 +4,20 @@ import { FilterPersonComponent } from './filter-person.component';
 import { AccordionModule } from 'ngx-accordion';
 import { MaterialModule, MdDatepickerModule, MdNativeDateModule } from '@angular/material';
 import { CheckBoxModule } from '../../check-box/check-box.module';
+import { DataPickerFilterModule } from '../../data-picker-filter/data-picker-filter.module';
+import { InputFilterModel } from '../../../interfaces/input-filter';
+import { InputFilterModule } from '../../input-filter/input-filter.module';
 
 @NgModule({
-  imports: [CommonModule,MaterialModule,AccordionModule,MdDatepickerModule, MdNativeDateModule,CheckBoxModule],
+  imports: [CommonModule,
+    MaterialModule,
+    AccordionModule,
+    MdDatepickerModule,
+    MdNativeDateModule,
+    CheckBoxModule,
+    DataPickerFilterModule,
+    InputFilterModule,
+  ],
   declarations: [FilterPersonComponent],
   exports: [FilterPersonComponent],
 })
--- src/app/main-page/person-list-component/person-list.component.ts
+++ src/app/main-page/person-list-component/person-list.component.ts
@@ -15,18 +15,112 @@ export class PersonListComponent implements OnInit {
   persons: CandidateCardItem [] = [];
   urlAdress: string;
   listItem: CardList;
+  body: any;
+  arrayOfCities: any[];
+  arrayOfSkills: any[];
+  arrayOfStatuses: any[];
+  arrayOfLanguages: any[];
+  arrayOfSalary: any[];
+  waitArrayOfSalary: any[];
+  date: Date;
 
-  constructor(private httpService: HttpService,private route:ActivatedRoute) {
+
+  constructor(private httpService: HttpService, private route: ActivatedRoute) {
+    this.arrayOfSkills = [];
+    this.arrayOfStatuses = [];
+    this.arrayOfLanguages = [];
+    this.arrayOfCities = [];
+    this.arrayOfSalary = [];
+    this.waitArrayOfSalary = [-1,-1];
+    this.date = new Date();
   }
 
-
   ngOnInit() {
-    this.httpService.postData({ skip: 0 }, 'http://localhost:1337/api/candidates')
+    this.httpService.postData({skip: 0}, 'http://localhost:1337/api/candidates')
       .subscribe((res) => {
         this.persons = res.json();
-        this.listItem = new CardList(this.persons,'candidates');
+        this.listItem = new CardList(this.persons, 'candidates');
         this.urlAdress = this.route.snapshot.url[0].path;
       });
+  }
 
+  send(event) {
+    // console.log(event);
+    this.checkGroupOfEvent(event);
+    console.log(this.arrayOfCities);
+    console.log(this.arrayOfStatuses);
+    console.log(this.arrayOfLanguages);
+    console.log(this.arrayOfSkills);
+    console.log(this.date);
+    console.log(this.arrayOfSalary);
+    this.body = {
+      skip: 0,
+      city: this.arrayOfCities,
+      status: this.arrayOfStatuses,
+      primarySkill: this.arrayOfSkills,
+      englishLvl: this.arrayOfLanguages,
+      salaryWish: this.arrayOfSalary,
+      expYear: this.date,
+    };
+    this.httpService.postData(this.body, 'http://localhost:1337/api/candidates')
+      .subscribe((res) => {
+        // console.log(res.json());
+        this.persons = res.json();
+        this.listItem = new CardList(this.persons, 'candidates');
+        this.urlAdress = this.route.snapshot.url[0].path;
+      });
+  }
+
+  isElemOfArray(array: any[], number: number): boolean {
+    let temp = array.indexOf(number);
+    if (temp > -1) {
+      array.splice(temp, 1);
+      return true;
+    }
+    else
+      return false;
+  }
+
+  checkGroupOfEvent(event) {
+    // console.log(typeof event.group);
+    switch (event.group) {
+      case 'city':
+        if (!this.isElemOfArray(this.arrayOfCities, event.id)) {
+          this.arrayOfCities.push(event.id);
+        }
+        break;
+      case 'skill':
+        if (!this.isElemOfArray(this.arrayOfSkills, event.id)) {
+          this.arrayOfSkills.push(event.id);
+        }
+        break;
+      case 'language':
+        if (!this.isElemOfArray(this.arrayOfLanguages, event.id)) {
+          this.arrayOfLanguages.push(event.id);
+        }
+        break;
+      case 'status':
+        if (!this.isElemOfArray(this.arrayOfStatuses, event.id)) {
+          this.arrayOfStatuses.push(event.id);
+        }
+        break;
+      case 'date':
+        this.date = event.id;
+        break;
+      case 'salary':
+        if(event.id.field === 'begin'){
+          this.waitArrayOfSalary[0] =  +event.id.event;
+        }
+        if(event.id.field === 'end'){
+          this.waitArrayOfSalary[1] =  +event.id.event;
+        }
+        console.log(this.waitArrayOfSalary);
+        if(this.waitArrayOfSalary[0]!= -1 && this.waitArrayOfSalary[1]!=-1 && this.waitArrayOfSalary[1]!= ''){
+          for (let i  = 0; i < 2; i++)
+          this.arrayOfSalary[i] = this.waitArrayOfSalary[i];
+        }
+        console.log(this.arrayOfSalary);
+        break;
+    }
   }
 }
