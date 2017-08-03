88a4731eac62ebf2dbc6e8b76f5ea5c1afcf67d8
Maxim Aderiha <maxima290898@gmail.com>
2017-08-03 18:06:47 +0300

    Other changes.

--- src/app/candidate-detail-wrapper/interview/interview.component.ts
+++ src/app/candidate-detail-wrapper/interview/interview.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { InterviewCard } from '../../classes/interview-card';
 
 @Component({
@@ -6,6 +6,16 @@ import { InterviewCard } from '../../classes/interview-card';
   templateUrl: 'interview.component.html',
   styleUrls: ['interview.component.scss'],
 })
-export class InterviewComponent {
+export class InterviewComponent implements OnInit {
   @Input() item: InterviewCard;
+  time: string;
+
+  ngOnInit() {
+    this.getTime();
+  }
+
+  getTime() {
+    const minutes = this.item.date.getMinutes() === 0 ? '00' : this.item.date.getMinutes().toString();
+    this.time = `${this.item.date.getHours()}:${minutes}`;
+  }
 }
